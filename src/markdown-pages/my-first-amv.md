317dbb4fc8ebfe2b51c6b8347ccb6d2c488586fe
Harshit Mahajan <hmahajan1999@gmail.com>
2019-10-20 14:03:02 +0530

    added code for gatsby setup

---
+++ src/markdown-pages/my-first-amv.md
@@ -0,0 +1,9 @@
+---
+title: 'My First AMV'
+date: '2017-08-21'
+description: 'My first AMV :)'
+---
+
+Check this out!
+
+<iframe width="673" height="538" src="https://www.youtube.com/embed/2-dohnsyL9I" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,67 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const Title = styled.h1`
+  display: inline-block;
+`
 
-export default IndexPage
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  &:hover {
+    color: #1dcaff;
+  }
+`
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const BlogBody = styled.div`
+  margin-bottom: 50px;
+`
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <Title>Thoughts by Harshit</Title>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <BlogBody key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.frontmatter.description || node.excerpt}</p>
+          </BlogBody>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            description
+          }
+          fields {
+            slug
+          }
+          excerpt(truncate: true)
+        }
+      }
+    }
+  }
+`
